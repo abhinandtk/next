5e4553e49fcc5b214bd37b30ee44c7b414931d3b
abhinandtk.primalcodes <abhinandtk.primalcodes@gmail.com>
2025-01-06 11:15:56 +0530

    env domain access

--- app/page.js
+++ app/page.js
@@ -2,9 +2,10 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   return (
     <main className={styles.main}>
-     New changesssss
+       <p>API URL: {apiUrl}</p>
     </main>
   )
 }
